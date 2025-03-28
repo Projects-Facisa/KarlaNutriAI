7082ebdf5cfa533c4268a43543dec0621ac455bd
Yuri-Diego <contatoprofissional.ya@gmail.com>
2025-03-28 00:12:00 -0300

    hotfix: standardized endpoints

--- karlanutriai-backend/routes.js
+++ karlanutriai-backend/routes.js
@@ -3,14 +3,17 @@ import AuthRouter from "./middleware/authRouter.js";
 import UserRouter from "./routes/userRouter.js"
 import NutritionalDataRouter from "./routes/nutritionalDataRouter.js";
 import MealRouter from "./routes/mealRouter.js"
+
 import {tokenValidate} from "./middleware/Auth.js";
+
 
 const router = express.Router();
 
-router.use("/auth", AuthRouter);
-router.use("/user", UserRouter);
-router.use("/data", tokenValidate, NutritionalDataRouter);
-router.use("/meal", tokenValidate, MealRouter);
+router.use("/auths", AuthRouter);
+router.use("/users", UserRouter);
+router.use("/datas", tokenValidate, NutritionalDataRouter);
+router.use("/meals", tokenValidate, MealRouter);
+
 
 
 export default router;

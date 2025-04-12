bca06f1ef7a0d8e4f5c3aa130889824e0baebaba
Yuri-Diego <contatoprofissional.ya@gmail.com>
2025-04-12 00:36:05 -0300

    feat: mealPhraseService with addMealPhrase and getMealPhrases

---
+++ karlanutriai-backend/services/mealPhrasesService.js
@@ -0,0 +1,34 @@
+import MealPhrases from "../models/MealPhrases.js";
+
+class mealPhrasesService {
+    async addMealPhrase(userId, phrase) {
+        try {
+            return await MealPhrases.findOneAndUpdate(
+            { userId },
+            { 
+                $push: { 
+                phrases: {
+                    $each: [phrase],
+                    $position: 0  // Adiciona no início do array
+                } 
+                }
+            },
+            { 
+                upsert: true,
+                new: true,
+                setDefaultsOnInsert: true 
+            }
+            );
+        } catch (error) {
+            console.error("Erro ao adicionar meal phrase:", error);
+            throw new Error("Falha ao atualizar meal phrases");
+        }
+    }
+    
+    async getMealPhrases(userId) {
+        const doc = await MealPhrases.findOne({ userId });
+        return doc ? doc.phrases : [];
+    }
+}
+
+export default new mealPhrasesService();
